1607f8126ea921f7767a933b0ebc82d8bfd68e70
khidd <kennedynyambura5981@gmail.com>
2023-06-12 16:47:10 +0300

    init

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,13 +1,32 @@
 <template>
   <p>the reaction time is {{ score }}ms</p>
+  <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
 export default {
-    props:['score']
-}
+    props:['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score < 250){
+            this.rank = 'ninja fingers'
+         } else if(this.score <400){
+            this.rank = 'Rapid reflexes'
+         }else{
+            this.rank = 'snail paces...'
+         }
+        }
+    }
 </script>
 
 <style>
-
+.rank{
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+}
 </style>
